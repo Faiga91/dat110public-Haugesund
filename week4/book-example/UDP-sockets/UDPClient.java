8a0ac4b5317054602a29743852b60c83f4132ad5
Faiga Alawad <faiga.alawd91@gmail.com>
2026-01-22 14:28:08 +0100

    Added UDP example from the book

---
+++ week4/book-example/UDP-sockets/UDPClient.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.net.*;
+
+public class UDPClient{
+
+    private static String HOST = "localhost";
+    private static int PORT = 8081;
+
+    public static void main(String argv[]) throws Exception
+        {
+        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+        DatagramSocket clientSocket = new DatagramSocket();
+        InetAddress IPAddress = InetAddress.getByName(HOST);
+        byte[] sendData = new byte[1024];
+        byte[] receiveData = new byte[1024];
+        String sentence = inFromUser.readLine();
+        sendData = sentence.getBytes();
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, PORT);
+        clientSocket.send(sendPacket);
+        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+        clientSocket.receive(receivePacket);
+        String modifiedSentence = new String(receivePacket.getData());
+        System.out.println("FROM SERVER:" + modifiedSentence);
+        clientSocket.close();
+    }
+    }
